baf34503cd97ac8de27e231116b7c7499f88c29e
Diana Jordan <dianacjordan@gmail.com>
2023-04-05 21:57:43 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+
+
+class Anagram:
+    
+    def __init__(self,word):
+        self.word=word
+    
+    def match(self,anagrams):
+        matches=[]
+        for word in anagrams:
+            if sorted(self.word) == sorted(word):
+                matches.append(word)
+            
+        return matches
